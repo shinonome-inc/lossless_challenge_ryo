d1dcc4640c322a2fd3f1a8bb7eaa2d338b3265db
Sota6174 <t119141som110223@gmail.com>
2020-07-14 08:18:16 +0900

    filterのdocstring修正

--- codec/filter.py
+++ codec/filter.py
@@ -1,5 +1,13 @@
 def sub_filter(array, height, width):
-    """Sub：２列目以降のピクセル値を左のピクセル値との差で置き換える"""
+    """Apply a sub_filter.
+
+    Parameters
+    ----------
+    array: ndarray
+    height: int
+    width: int
+
+    """
     for y in range(height):
         for x in range(width - 1, 0, -1):
             array[y][x] = int(array[y][x]) - int(array[y][x-1])
@@ -7,7 +15,15 @@ def sub_filter(array, height, width):
 
 
 def up_filter(array, height, width):
-    """Up：二行目以降のピクセル値を真上のピクセル値との差で置き換える"""
+    """Apply a up_filter.
+
+    Parameters
+    ----------
+    array: ndarray
+    height: int
+    width: int
+
+    """
     for y in range(height-1, 0, -1):
         for x in range(width):
             array[y][x] = int(array[y][x]) - int(array[y-1][x])
